345e04c803517ab5bdb10d1db66c42b94b92ad45
YueZhengMeng <804570270@qq.com>
2021-12-23 00:19:36 +0800

    增加了jar包一键部署和docker相关命令

--- src/main/java/com/shou/socialpracticemanager/config/SwaggerConfig.java
+++ src/main/java/com/shou/socialpracticemanager/config/SwaggerConfig.java
@@ -18,7 +18,6 @@ public class SwaggerConfig {
         Docket docket = new Docket(DocumentationType.SWAGGER_2);
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("SocialPracticeManager 接口文档")
-                .description("登录请求格式举例：POST http://localhost:8080/api/login?username=pwb&password=pwb")
                 .version("2.0")
                 .build();
 
